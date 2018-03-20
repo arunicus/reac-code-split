ae170750d356d947beb813a817cc565dcdf75520
Arun Venkatesh <avenkatesh@LNHOLDINGS.COM>
2018-03-20 17:47:25 -0400

    Adding few css

--- src/components/inventions/Invention.jsx
+++ src/components/inventions/Invention.jsx
@@ -4,12 +4,21 @@ import './Invention.css';
 export default function Invention({ name }) {
     return (
         <div class="card">
+            <img src="r2d2.png" alt="Avatar" class="avatar" />
             <div class="container">
                 <p>  
-                    <img src="r2d2.png" alt="Avatar" class="avatar" />
                     <b>testt</b> <br /> 
                     Architect & EngineerArchitect & EngineerArchitect & EngineerArchitect & EngineerArchitect & EngineerArchitect & Engineer
                 </p>
+            </div>
+            <hr class="light"/>
+            <div style={{padding: '8px'}}>
+                <span style={{float: 'right'}}>
+                    <i className="heart outline like icon"></i>
+                    17 likes
+                </span>
+                    <i className="comment icon"></i>
+                    3 comments
             </div>
         </div>
     );
